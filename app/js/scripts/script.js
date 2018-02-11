722d846a90a14e0807c9215fe7e0eb15aabc85da
swagata-kundu <swagatakr.kundu@gmail.com>
2018-02-11 23:12:31 +0530

    Askwheel bug fix M3  1

--- app/js/scripts/script.js
+++ app/js/scripts/script.js
@@ -1,6 +1,10 @@
 var showErrorMessage = function(error) {
   if (error.status == 400) {
-    bootbox.alert("Bad request. Unable to process!");
+    if (error.data && error.data.details && error.data.details.length > 0) {
+      bootbox.alert(error.data.details[0].errors[0]);
+    } else {
+      bootbox.alert("Bad request. Unable to process!");
+    }
   } else {
     if (error && error.data && error.data.details && error.data.details[0])
       bootbox.alert(
@@ -11,4 +15,3 @@ var showErrorMessage = function(error) {
     else bootbox.alert("Something went wrong with server!");
   }
 };
-
